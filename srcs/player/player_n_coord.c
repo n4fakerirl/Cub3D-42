b9d05f7c6c041e2f0ae5feec1ef820b5a9388739
ocviller <ocviller@student.42.fr>
2026-02-17 10:08:22 +0100

    txt old a jour + diminitution de la hitbox

--- srcs/player/player_n_coord.c
+++ srcs/player/player_n_coord.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player_n_coord.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gule-bat <gule-bat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 18:15:58 by gule-bat          #+#    #+#             */
-/*   Updated: 2026/02/09 21:45:56 by gule-bat         ###   ########.fr       */
+/*   Updated: 2026/02/17 10:04:21 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	coord_add_or_sub_x(t_data *data, float x, float y, int side)
 {
 	float	hb;
 
-	hb = 0.8;
+	hb = 0.3;
 	if (((contact(data, data->player.p_x + x + (hb), data->player.p_y + y, 0)
 				&& side == 0) || (contact(data, data->player.p_x + x - (hb),
 					data->player.p_y + y, 0) && side == 0))
